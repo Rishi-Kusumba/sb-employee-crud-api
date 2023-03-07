030aef189d7e9a9a35947416b44945978d265526
Rishi147 <kusumba.rishi@gmail.com>
2023-03-07 20:31:05 +0530

    Initial commit

---
+++ employee-crud-api/src/main/java/dev/projects147/employeecrudapi/EmployeeCrudApiApplication.java
@@ -0,0 +1,13 @@
+package dev.projects147.employeecrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudApiApplication.class, args);
+	}
+
+}
---
+++ employee-crud-api/src/test/java/dev/projects147/employeecrudapi/EmployeeCrudApiApplicationTests.java
@@ -0,0 +1,8 @@
+package dev.projects147.employeecrudapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudApiApplicationTests {
+
+}
